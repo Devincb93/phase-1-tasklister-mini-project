97c9f6ab6976b158735aa92f58baa3b528a810e8
Devin <devincb93@gmail.com>
2023-07-23 15:14:15 -0700

    TaskLister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const taskDescription = document.getElementById('new-task-description')
+    const addToList = document.getElementById('tasks')
+    const newTask = document.createElement('li')
+    newTask.innerText = taskDescription.value
+    addToList.appendChild(newTask)
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x'
+    newTask.appendChild(btn)
+    form.reset()
+
+
+  })
+  function handleDelete(e){
+    e.target.parentNode.remove();
+  }
 });
